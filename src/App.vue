15134e0b0ca162115e10cef7bf687e5d9e833cfe
Anton Tigunov <seyanaracore@gmail.com>
2022-07-21 03:51:14 +0300

    app styles, mobile request

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,36 @@ export default {
 #app {
   max-width: 1300px;
   padding: 0 12px;
+  margin: 100px auto 12px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  color: $blackColor;
+  font-family: "VelaSansRegular", sans-serif;
+  &:focus-visible {
+    outline: none;
+  }
+}
+button {
+  cursor: pointer;
+}
+@font-face {
+  font-family: "MontserratBold";
+  src: url("./fonts/Montserrat-Bold.ttf");
+  font-style: normal;
+  font-weight: bold;
+}
+@font-face {
+  font-family: "VelaSansRegular";
+  src: url("./fonts/VelaSans-Regular.ttf");
+  font-style: normal;
+  font-weight: normal;
+}
+
+@media (max-width: $mobileWrap) {
+  #app {
+    margin-top: 24px;
+  }
 }
 </style>
