8710b5b8d2e085f6d220c4ed90047b3f80a431c9
Anton Tigunov <seyanaracore@gmail.com>
2022-07-21 03:42:45 +0300

    Sort Todo list

---
+++ src/components/SortTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="sort-container">
+    <p class="sort__title">Сортировать&nbsp;по:</p>
+    <!-- <select v-model="sortBy" id="sort-by">
+      <option v-for="(sortType, idx) in sortingTypes" :key="idx">
+        {{ sortType.title }}
+      </option>
+    </select> -->
+    <!-- Thanks Safari :) -->
+    <custom-select
+      class="sort__select"
+      :options="sortingTypes"
+      @input="sortBy"
+    />
+  </div>
+</template>
+
+<script>
+import CustomSelect from "./UI/CustomSelect.vue";
+export default {
+  components: { CustomSelect },
+
+  data() {
+    return {
+      sortingTypes: ["Дата", "Статус"],
+    };
+  },
+
+  methods: {
+    sortBy(option) {
+      this.$store.commit("setSort", option);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/constants.scss";
+.sort__title {
+  margin-right: 15px;
+}
+.sort-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+@media (max-width: $mobileWrap) {
+  .sort-container {
+    margin-top: 12px;
+    align-self: center;
+  }
+}
+</style>
