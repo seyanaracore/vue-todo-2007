6de7bca422ce5206355d7e66a878f5a5690521b5
Anton Tigunov <seyanaracore@gmail.com>
2022-07-21 03:35:36 +0300

    default todo list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,54 @@
 export const LOCAL_TODOS_KEY = "todoList";
 export const API_URL = "https://jsonplaceholder.typicode.com/todos/";
 export const TODOS_FETCHING_COUNT = 10;
+export const DEFAULT_TODO_LIST = [
+  {
+    completed: true,
+    id: 1,
+    title: "Размещение новостей на сайте",
+    date: 1650574800000,
+  },
+  {
+    completed: false,
+    id: 2,
+    title: "Внедрить Wi-fi для читателей",
+    date: 1648155600000,
+  },
+  {
+    completed: true,
+    id: 3,
+    title: "Отредактировать раздел “Доступная среда”",
+    date: 1647291600000,
+  },
+  {
+    completed: false,
+    id: 4,
+    title: "Презентация “Информационные технологии”",
+    date: 1647291600000,
+  },
+  {
+    completed: false,
+    id: 5,
+    title: "Счётчики — внедрить дизайн",
+    date: 1646773200000,
+  },
+  {
+    completed: false,
+    id: 6,
+    title: "Сверстать новый layout",
+    date: 1646600400000,
+  },
+  { completed: true, id: 7, title: "Скролл в новостях", date: 1646082000000 },
+  {
+    completed: false,
+    id: 8,
+    title: "Форма сброса пароля",
+    date: 1645736400000,
+  },
+  {
+    completed: true,
+    id: 9,
+    title: "Внедрение модуля Chat",
+    date: 1645304400000,
+  },
+];
